d11fb9a823ed195eb4c094e91665b879ccf34fc6
Jishan Ahamed <jjahamed33@gmail.com>
2023-10-01 00:00:32 +0530

    many to many actor-movie, Setup DbSets

--- eTicket/Data/AppDbContext.cs
+++ eTicket/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using eTicket.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace eTicket.Data
 {
@@ -7,5 +8,34 @@ namespace eTicket.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Defining the Composite Key
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new { 
+                am.ActorId, 
+                am.MovieId
+            });
+
+            //Defining one to many relation between actor - Actor_Movie
+            modelBuilder.Entity<Actor_Movie>()
+                .HasOne(a => a.Actor)
+                .WithMany(am => am.Actors_Movies)
+                .HasForeignKey(am => am.ActorId);
+            //Defining one to many relation between movie - Actor_Movie
+            modelBuilder.Entity<Actor_Movie>()
+                .HasOne(m => m.Movie)
+                .WithMany(am => am.Actors_Movies)
+                .HasForeignKey(am => am.MovieId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+
     }
 }
