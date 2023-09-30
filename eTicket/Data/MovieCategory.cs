0e0d68903f106c1fd0c3610a5165c8cc94513448
Jishan Ahamed <jjahamed33@gmail.com>
2023-09-30 20:19:31 +0530

    Added MovieCategory enum in Data folder

---
+++ eTicket/Data/MovieCategory.cs
@@ -0,0 +1,15 @@
+﻿namespace eTicket.Data
+{
+    public enum MovieCategory
+    {
+        Action=1,
+        Adventure,
+        Comedy,
+        Documentary,
+        Drama,
+        Horror,
+        Mystery,
+        Romance,
+        Thriller,
+    }
+}
