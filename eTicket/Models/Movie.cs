6bcc410f45eea8046ec2c85df05381cfa8e6b170
Jishan Ahamed <jjahamed33@gmail.com>
2023-09-30 20:16:46 +0530

    Added Movie entity model

---
+++ eTicket/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTicket.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime StarDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
