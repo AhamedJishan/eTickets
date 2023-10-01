de246262624a1e7587223cd251e5a2796048ef89
Jishan Ahamed <jjahamed33@gmail.com>
2023-10-02 01:06:23 +0530

    producers view

--- eTicket/Models/Producer.cs
+++ eTicket/Models/Producer.cs
@@ -4,13 +4,17 @@ namespace eTicket.Models
 {
     public class Producer
     {
-        [Key]
-        public int Id { get; set; }
-        public string ProfilePictureURL { get; set; }
-        public string FullName { get; set; }
-        public string Bio { get; set; }
+		[Key]
+		public int Id { get; set; }
 
-        //Relationships
-        public List<Movie> Movies { get; set; }
+		[Display(Name = "Profile Picture")]
+		public string ProfilePictureURL { get; set; }
+
+		[Display(Name = "Full Name")]
+		public string FullName { get; set; }
+		public string Bio { get; set; }
+
+		//Relationships
+		public List<Movie> Movies { get; set; }
     }
 }
