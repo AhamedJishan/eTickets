45e3cd385e6f04d8881a1be8e0d9e2b14e3b0ac0
Jishan Ahamed <jjahamed33@gmail.com>
2023-10-01 23:17:38 +0530

    Added actors and Producers controller

---
+++ eTicket/Controllers/ActorsController.cs
@@ -0,0 +1,21 @@
+﻿using eTicket.Data;
+using eTicket.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+namespace eTicket.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _db;
+        public ActorsController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Actor> actors = _db.Actors.ToList();
+            return View();
+        }
+    }
+}
---
+++ eTicket/Controllers/ProducersController.cs
@@ -0,0 +1,19 @@
+﻿using eTicket.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTicket.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _db;
+        public ProducersController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
