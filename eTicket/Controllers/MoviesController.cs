40590447287ad014e96338fe748a6c8adfaf42d1
Jishan Ahamed <jjahamed33@gmail.com>
2023-10-01 23:21:34 +0530

    Movies Controller

---
+++ eTicket/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using eTicket.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTicket.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _db;
+        public MoviesController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _db.Movies.ToListAsync();
+            return View();
+        }
+    }
+}
