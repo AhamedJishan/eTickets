4084a803c189b126af64fa633a8487fe7233750a
Jishan Ahamed <jjahamed33@gmail.com>
2023-10-01 23:19:04 +0530

    CinemasController

---
+++ eTicket/Controllers/CinemasController.cs
@@ -0,0 +1,18 @@
+﻿using eTicket.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTicket.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AppDbContext _db;
+        public CinemasController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
