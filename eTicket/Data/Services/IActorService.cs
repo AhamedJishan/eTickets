725f2b13baf82f6150851da4827c1c595390f830
Jishan Ahamed <jjahamed33@gmail.com>
2023-10-02 22:53:00 +0530

    IActorService

---
+++ eTicket/Data/Services/IActorService.cs
@@ -0,0 +1,13 @@
+﻿using eTicket.Models;
+
+namespace eTicket.Data.Services
+{
+	public interface IActorService
+	{
+		IEnumerable<Actor> GetAllActors();
+		Actor GetActor(int id);
+		void AddActor(Actor actor);
+		void RemoveActor(int id);
+		Actor UpdateActor(int id, Actor newActor);
+	}
+}
